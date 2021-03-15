4b8fb3554e8dba2e813f5a2b97b2f457448fdf36
JorisStoppelenburg <75532530+JorisStoppelenburg@users.noreply.github.com>
2021-03-15 12:20:06 +0100

    Merge branch 'main' of https://github.com/patrick10202/Project-B into main

---
+++ testclass.cs
@@ -0,0 +1,18 @@
+using system;
+/*class Screen{
+    if (test = 1){
+        console.WriteLine("test1");
+    } else if ( test = 2) {
+        console.WriteLine("test2");
+    } else {
+        break;
+    }
+}*/
+class Program{
+    static void Main(){
+        while (true){
+        console.WriteLine("fakka");
+        test = console.ReadLine();
+        }
+    }
+}
