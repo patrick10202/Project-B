bc4e54721577a8b348e0526a9b067f13918352df
patrick10202 <Patrickspekman@icloud.com>
2021-03-15 13:19:50 +0100

    Update Program.cs

--- C#/Program.cs
+++ C#/Program.cs
@@ -6,7 +6,7 @@ namespace C_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
