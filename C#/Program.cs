00c064069be277dab781af77f47fff85be15349c
Stephan van den Berg <75629962+steef-010@users.noreply.github.com>
2021-03-16 15:46:04 +0100

    Update Program.cs

--- C#/Program.cs
+++ C#/Program.cs
@@ -31,19 +31,19 @@ class program{
             } else if (UserInput == "2"){
                 Screen.MovieScreen();
                 // login screen
-                Console.WriteLine("u typt 1");
+                Console.WriteLine("you typed in 1");
             } else if (UserInput == "2"){
                 // movies screen
-                Console.WriteLine("u typt 2");
+                Console.WriteLine("you typed in 2");
             } else if (UserInput == "3"){
                 // reserve screen
-                Console.WriteLine("u typt 3");
+                Console.WriteLine("you typed in 3");
             } else if (UserInput == "4"){
                 // coming soon screen
-                Console.WriteLine("u typt 4");
+                Console.WriteLine("you typed in 4");
             } else if (UserInput == "5"){
                 // info screen
-                Console.WriteLine("u typt 5");
+                Console.WriteLine("you typed in 5");
             } else if (UserInput == "0"){
               break;
             }
