40588c915c22091bbddf06cb44311d41ca1b5047
JorisStoppelenburg <75532530+JorisStoppelenburg@users.noreply.github.com>
2021-03-16 15:56:17 +0100

    test

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,25 +1,20 @@
 ﻿using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-/*class Program {
-  public static void Main(){
-      weatherForecast = JsonSerializer.Deserialize<test>(jsonString);
-  }
-}
-
-class Scherm {
-
-}*/
 
 public class Screen{
-    public static void HomeScreen(string ding){
-        Console.WriteLine($"hallo, u typt: {ding}");
+    public static void HomeScreen(){
+        Console.WriteLine("HomeScreen");
+    }
+    public static void MovieScreen(){
+        Console.WriteLine("MovieScreen");
     }
 }
 
 public class Phrases{
     public static string inputPlease(){
         Console.WriteLine("Please input a number to see the following menus:");
+        Console.WriteLine("1: HomeScreen\n2: movies\n3: reserve\n4: comming soon\n5:info");
         Console.WriteLine("1: login\n2: movies\n3: reserve\n4: coming soon\n5: info");
         Console.WriteLine("to quit, enter 0");
         string UserInput = Console.ReadLine();
@@ -32,20 +27,23 @@ class program{
         while (true) {
             string UserInput = Phrases.inputPlease();
             if (UserInput == "1"){
+                Screen.HomeScreen();
+            } else if (UserInput == "2"){
+                Screen.MovieScreen();
                 // login screen
-                Console.WriteLine("u typt 1");
+                Console.WriteLine("you typed in 1");
             } else if (UserInput == "2"){
                 // movies screen
-                Console.WriteLine("u typt 2");
+                Console.WriteLine("you typed in 2");
             } else if (UserInput == "3"){
                 // reserve screen
-                Console.WriteLine("u typt 3");
+                Console.WriteLine("you typed in 3");
             } else if (UserInput == "4"){
                 // coming soon screen
-                Console.WriteLine("u typt 4");
+                Console.WriteLine("you typed in 4");
             } else if (UserInput == "5"){
                 // info screen
-                Console.WriteLine("u typt 5");
+                Console.WriteLine("you typed in 5");
             } else if (UserInput == "0"){
               break;
             }
