77b85cffbf4ae8f41c31c7f33d445f368941aa2f
JorisStoppelenburg <75532530+JorisStoppelenburg@users.noreply.github.com>
2021-03-16 15:19:38 +0100

    input werkt

--- C#/Program.cs
+++ C#/Program.cs
@@ -16,10 +16,37 @@ public class Screen{
         Console.WriteLine($"hallo, u typt: {ding}");
     }
 }
+
+public class Phrases{
+    public static string inputPlease(){
+        Console.WriteLine("Please input a number to see the following menus:");
+        Console.WriteLine("1: login\n2: movies\n3: reserve\n4: comming soon\n5:info");
+        Console.WriteLine("to quit, enter 0");
+        string UserInput = Console.ReadLine();
+        return UserInput;
+    }
+}
 class program{
     static void Main(){
-        Console.WriteLine("typ iets");
-        string usrinput = Console.ReadLine();
-        Screen.HomeScreen(usrinput);
+        Console.WriteLine("Welcome to our cinema app");
+        while (true) {
+            string UserInput = Phrases.inputPlease();
+            if (UserInput == "1"){
+                Console.WriteLine("u typt 1");
+            } else if (UserInput == "2"){
+                Console.WriteLine("u typt 2");
+            } else if (UserInput == "3"){
+                Console.WriteLine("u typt 3");
+            } else if (UserInput == "4"){
+                Console.WriteLine("u typt 4");
+            } else if (UserInput == "5"){
+                Console.WriteLine("u typt 5");
+            }
+            Console.WriteLine("press enter to continue");
+            Console.ReadLine();
+
+        }
+        
+        
     }
 }
