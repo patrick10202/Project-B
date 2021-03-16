4f1acb4df9ba8de83ac34401b2da5baa715b1881
patrick10202 <Patrickspekman@icloud.com>
2021-03-16 14:21:21 +0100

    .

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-class Screen{
-    if (test = 1){
-        Console.WriteLine("test1");
-    } else if ( test = 2) {
-        Console.WriteLine("test2");
-    } else {
-        break;
-    }
-}
-
 namespace testingeningen
 {
-
 class Program{
     static void Main(){
         while (true){
-        Console.WriteLine("fakka");
-        test = Console.ReadLine();
+        Console.WriteLine("test");
         }
-
+        }
     }
 }
