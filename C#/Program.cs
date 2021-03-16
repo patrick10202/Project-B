0e77a9b380cc55163da24379926f29366d3b0be3
patrick10202 <Patrickspekman@icloud.com>
2021-03-16 14:49:00 +0100

    class scherm

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
+using System.Text.Json;
+using System.Text.Json.Serialization;
 class Program {
   public static void Main(){
-      
+      weatherForecast = JsonSerializer.Deserialize<test>(jsonString);
   }
 }
 
 class Scherm {
-    
+
 }
